3b9f59d156cd91d198b2e7219954b42663315cfe
ksborra027 <kaavyaborra@gmail.com>
2023-03-05 05:56:20 -0500

    amend 2 fr

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def home_route() -> tuple[dict[str, any], int]:
     fish: str = request.args.get('q')
-    fishies = ["atlantic-sturgeon", "blackbanded-sunfish", "blackside-dace","candy-darter","carolina-darter", "clinch-dace", "duskytail-darter", "emerald-shiner", "golden-darter", "greenfin-darter", "orangefin-madtom", "paddlefish-", "roanoke-logperch", "sharphead-darter", "shortnose-sturgeon", "sickle-darter", "slender-chub", "spotfin-chub", "steelcolor-shiner", "tennessee-dace", "variegate-darter", "western-sand-darter", "whitemouth-shiner", "yellowfin-madtom"]
+    fishies = ["atlantic-sturgeon", "blackbanded-sunfish", "blackside-dace","candy-darter","carolina-darter", "clinch-dace", "duskytail-darter", "emerald-shiner", "golden-darter", "greenfin-darter", "orangefin-madtom", "paddlefish", "roanoke-logperch", "sharphead-darter", "shortnose-sturgeon", "sickle-darter", "slender-chub", "spotfin-chub", "steelcolor-shiner", "tennessee-dace", "variegate-darter", "western-sand-darter", "whitemouth-shiner", "yellowfin-madtom"]
     if fish in fishies:
             return render_template(fish + ".html")
     return render_template("main.html")
